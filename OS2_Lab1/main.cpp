b133c33848e5ad38c4d15ddb6de1887a118012e9
EzCoder <EzCoder@iMac-EzCoder.local>
2014-12-23 22:06:46 +0300

    Initial Commit

---
+++ OS2_Lab1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OS2_Lab1
+//
+//  Created by EzCoder on 23.12.14.
+//  Copyright (c) 2014 EzCoder. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
