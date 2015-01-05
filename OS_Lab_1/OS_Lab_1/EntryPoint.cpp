666ed2ddc85b9ecdee2a34dbe5780ded33844b01
EzCoder <tigran.avsharyan@gmail.com>
2015-01-05 18:56:10 +0300

    :)

--- OS_Lab_1/OS_Lab_1/EntryPoint.cpp
+++ OS_Lab_1/OS_Lab_1/EntryPoint.cpp
@@ -1,6 +1,30 @@
+#include <iostream>
+#include <vector>
+#define MEM_SIZE 10
+using namespace std;
+struct mem_block;
+vector<mem_block> memory;
+
+struct mem_block
+{
+	bool	lock;
+	void*	data;
+};
+
+void ClearMemory()
+{
+	for (size_t i = 0; i < memory.size(); i++)
+	{
+		memory[i].data = nullptr;
+		memory[i].lock = false;
+	}
+}
 
 
 void main()
 {
+	memory.reserve(MEM_SIZE);
+
+
 
 }
