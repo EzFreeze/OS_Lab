cb2a9eca5d1eb49edcea309ccd6a6ad0bdfdbeba
EzCoder <tigran.avsharyan@gmail.com>
2015-02-03 22:03:41 +0300

    +

--- OS_Lab_3/OS_Lab_3/EntryPoint.cpp
+++ OS_Lab_3/OS_Lab_3/EntryPoint.cpp
@@ -3,41 +3,57 @@
 #include <stdexcept>
 
 using namespace std;
+/************************************************************************/
+/*                           »сходные данные                            */
+/************************************************************************/
+const int	cCyl	= 500;	/*количество цилиндров */
+const int	cGol	= 4;	/*количество поверхностей */
+const int	cSec	= 16;	/*количество секторов */
+const int	vSpeed	= 1000	/*об/мин */;
+const float tJump	= .5f;	/*врем€ перехода с дорожки на соседнюю дорожку */
+/************************************************************************/
 
 vector<int> gRequest;
+
+
 
 void init()
 {
 
 }
 
-class FIFO
+class HDD
 {
 public:
-	FIFO(vector<int> iRequest)
-	{
-		pRequest = iRequest;
-	}
-
-	//
-	void PrintResult()
-	{
-		for (size_t i = 0; i < pRequest.size(); i++)
-		{
-			cout << pRequest.at(i) << endl;
-		}
-	}
-
-protected:
+	/************************************************************************/
+	/*                           /«апрос к диску/                           */
+	/************************************************************************/
+	virtual void Request(
+		int nCyl	/*номер цилиндра*/,
+		int nGol	/*номер головки*/,
+		int nSec	/*номер сектора*/,
+		bool oType	/*тип операции(0 - чтение, 1 - запись)*/) = 0;
+	/************************************************************************/
 
 private:
 	vector<int> pRequest;
 };
 
+
+class FIFO:HDD
+{
+public:
+	virtual void Request(int nCyl, int nGol, int nSec, bool oType)
+	{
+
+	}
+private:
+
+};
+
+
 void main()
 {
-
-	FIFO* pFIFO = new FIFO(gRequest);
-	pFIFO->PrintResult();
+	FIFO* pFIFO = new FIFO();
 
 }
