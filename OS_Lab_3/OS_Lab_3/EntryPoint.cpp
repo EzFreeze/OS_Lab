a143602e93a295943e2ecb4a1b944326957b1a42
EzCoder <tigran.avsharyan@gmail.com>
2015-02-03 21:39:50 +0300

    +

--- OS_Lab_3/OS_Lab_3/EntryPoint.cpp
+++ OS_Lab_3/OS_Lab_3/EntryPoint.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
+#include <vector>
+#include <stdexcept>
 
+using namespace std;
+
+vector<int> gRequest;
+
+void init()
+{
+
+}
+
+class FIFO
+{
+public:
+	FIFO(vector<int> iRequest)
+	{
+		pRequest = iRequest;
+	}
+
+	//
+	void PrintResult()
+	{
+		for (size_t i = 0; i < pRequest.size(); i++)
+		{
+			cout << pRequest.at(i) << endl;
+		}
+	}
+
+protected:
+
+private:
+	vector<int> pRequest;
+};
 
 void main()
 {
+
+	FIFO* pFIFO = new FIFO(gRequest);
+	pFIFO->PrintResult();
 
 }
