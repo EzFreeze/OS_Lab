8e8eee5068304b913b58bd20dbb73b7391b11d7e
l1merain <tigran.avsharyan@gmail.com>
2015-02-04 18:49:55 +0300

    up

--- OS_Lab_3/OS_Lab_3/FIFO.h
+++ OS_Lab_3/OS_Lab_3/FIFO.h
@@ -4,7 +4,7 @@ namespace FIFO{
 	float exTime; // Время работы
 	
 	std::vector<sRequest> request;
-	sRequest curPost = { 1, 1, 1, 1 };
+	sRequest curPos = { 1, 1, 1, 1 };
 
 	void init(){
 		srand(clock());
@@ -22,28 +22,29 @@ namespace FIFO{
 		return ((static_cast<float>(rand()) / static_cast<float>(RAND_MAX)) * max);
 	}
 
-	void PrintVector()
-	{
-		for (size_t i = 0; i < request.size(); i++)
-		{
+	void PrintVector(){
+		for (size_t i = 0; i < request.size(); i++){
 			std::cout << request.at(i).nCyl << "|" << request.at(i).nGol << "|" << request.at(i).nSec << "|" << request.at(i).oType << std::endl;
 		}
 	}
 
 	void FIFO(){
 		init();
+		PrintVector();
 		try{
 			while (request.size() != 0)
 			{
-				pTime += RandFloat(2);// время простоя
-				exTime += abs(curPost.nCyl - request.back().nCyl)*tJump;//Время перехода между дорожками
+				pTime	+= RandFloat(2);										// время простоя
+				exTime	+= (abs(curPos.nCyl - request.back().nCyl))*tJump;		//Время перехода между дорожками
+				curPos	= request.back();
 
-				request.pop_back();//удалить элемент из массива
+				request.pop_back();												//удалить элемент из массива
 			}
 		}
 		catch (const std::out_of_range& e){
 			std::cout <<"Error: "<< e.what() << std::endl;
 		}
-		std::cout << "Время простоя: " << pTime << std::endl;
+		
+		std::cout << "Время простоя: " << pTime + (exTime/1000) << std::endl;
 	}
 }
