8373868e07c1a737dfaf7b2d89cb0a9e288585d5
l1merain <tigran.avsharyan@gmail.com>
2015-02-04 18:59:38 +0300

    up+

--- OS_Lab_3/OS_Lab_3/FIFO.h
+++ OS_Lab_3/OS_Lab_3/FIFO.h
@@ -1,8 +1,9 @@
 namespace FIFO{
-	size_t cRequest = 10;
-	float pTime;// Время простоя
-	float exTime; // Время работы
-	
+	size_t cRequest = 1;
+	float pTime;			// Время простоя
+	float exTime;			// Время работы
+	float MaxTime = 3600;	// Время моделирования
+
 	std::vector<sRequest> request;
 	sRequest curPos = { 1, 1, 1, 1 };
 
@@ -30,21 +31,31 @@ namespace FIFO{
 
 	void FIFO(){
 		init();
-		PrintVector();
 		try{
 			while (request.size() != 0)
 			{
-				pTime	+= RandFloat(2);										// время простоя
-				exTime	+= (abs(curPos.nCyl - request.back().nCyl))*tJump;		//Время перехода между дорожками
+				pTime	+= RandFloat(2);											// время простоя
+				exTime	+= ((abs(curPos.nCyl - request.back().nCyl))*tJump)/1000;	//Время перехода между дорожками (в секундах)
 				curPos	= request.back();
 
-				request.pop_back();												//удалить элемент из массива
+
+
+				request.pop_back();													//удалить элемент из массива
+				request.push_back({
+					(0 + rand() % cCyl),
+					(0 + rand() % cGol),
+					(0 + rand() % cSec),
+					((rand() % 2) ? true : false)
+				});
+
+				if ((pTime+exTime) > MaxTime) break;							   //проверка времени моделирования
 			}
 		}
 		catch (const std::out_of_range& e){
 			std::cout <<"Error: "<< e.what() << std::endl;
 		}
 		
-		std::cout << "Время простоя: " << pTime + (exTime/1000) << std::endl;
+		std::cout << "Время  работы: " << exTime	<< std::endl;
+		std::cout << "Время простоя: " << pTime		<< std::endl;
 	}
 }
