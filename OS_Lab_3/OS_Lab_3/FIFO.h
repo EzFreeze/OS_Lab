e64feb20e592b55994900dc88728cf0636f22335
l1merain <tigran.avsharyan@gmail.com>
2015-02-04 19:03:17 +0300

    +

--- OS_Lab_3/OS_Lab_3/FIFO.h
+++ OS_Lab_3/OS_Lab_3/FIFO.h
@@ -1,8 +1,8 @@
 ﻿namespace FIFO{
 	size_t cRequest = 1;
-	float pTime;			// Время простоя
-	float exTime;			// Время работы
-	float MaxTime = 3600;	// Время моделирования
+	float pTime;/*Время простоя*/
+	float exTime;/*Время работы*/
+	float MaxTime = 3600;/*Время моделирования*/
 
 	std::vector<sRequest> request;
 	sRequest curPos = { 1, 1, 1, 1 };
@@ -25,7 +25,12 @@
 
 	void PrintVector(){
 		for (size_t i = 0; i < request.size(); i++){
-			std::cout << request.at(i).nCyl << "|" << request.at(i).nGol << "|" << request.at(i).nSec << "|" << request.at(i).oType << std::endl;
+			std::cout 
+				<< request.at(i).nCyl << "|" 
+				<< request.at(i).nGol << "|" 
+				<< request.at(i).nSec << "|" 
+				<< request.at(i).oType 
+				<< std::endl;
 		}
 	}
 
@@ -34,13 +39,13 @@
 		try{
 			while (request.size() != 0)
 			{
-				pTime	+= RandFloat(2);											// время простоя
-				exTime	+= ((abs(curPos.nCyl - request.back().nCyl))*tJump)/1000;	//Время перехода между дорожками (в секундах)
+				pTime	+= RandFloat(2);/*время простоя*/
+				exTime	+= ((abs(curPos.nCyl - request.back().nCyl))*tJump)/1000;/*Время перехода между дорожками (в секундах)*/
 				curPos	= request.back();
 
 				//-
 
-				request.pop_back();													//удалить элемент из массива
+				request.pop_back();/*удалить элемент из массива*/
 				request.push_back({
 					(0 + rand() % cCyl),
 					(0 + rand() % cGol),
@@ -48,7 +53,7 @@
 					((rand() % 2) ? true : false)
 				});
 
-				if ((pTime+exTime) > MaxTime) break;								//проверка времени моделирования
+				if ((pTime+exTime) > MaxTime) break;/*проверка времени моделирования*/
 			}
 		}
 		catch (const std::out_of_range& e){
